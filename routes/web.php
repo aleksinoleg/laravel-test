aa037b97da993b6f6521c37cc84565f931202d04
oleg <aleksin.oleg@gmail.com>
2017-12-10 11:42:19 +0200

    delete trash

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', function (){
-	return view('about');
-});
 
-
-Route::get('contact', function (){
-	return view('contact');
-});
-
-
-/*some master routes*/
 
